523753a1d389267cf15bfa5d41ea73a1dae4e8e4
AmeRain <rainyblue@mail.ru>
2026-08-25 13:11:43 +0300

    fix env v.2

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,11 +3,11 @@ import { expect, test as setup } from '@playwright/test';
 const authFile = 'playwright/.auth/user.json';
 
 setup('авторизация', async ({ page }) => {
-  console.log(`URL: ${process.env.URL}`)
+  console.log(`URL: ${process.env.BASE_URL}`)
   await page.goto('/login');
 
-  await page.getByLabel('Email').fill(process.env.EMAIL);
-  await page.getByLabel('Пароль').fill(process.env.PASSWORD);
+  await page.getByLabel('Email').fill(process.env.TEST_EMAIL);
+  await page.getByLabel('Пароль').fill(process.env.TEST_PASSWORD);
   await page.getByRole('button', { name: 'Войти' }).click();
 
 
