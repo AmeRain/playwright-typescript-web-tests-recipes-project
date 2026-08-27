01424d785465d720e9d36bc5e64280b6e297e4de
AmeRain <rainyblue@mail.ru>
2026-08-27 10:20:46 +0300

    fix login test for mobile

--- tests/test-login.spec.ts
+++ tests/test-login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 // test('test auth faild', async ({ page }) => {
-//   await page.goto('https://wife-recipes.vercel.app/login');
+//   await page.goto('/login');
 //   await page.getByRole('textbox', { name: 'Email' }).click();
 //   await page.getByRole('textbox', { name: 'Email' }).fill('user');
 //   await page.getByRole('textbox', { name: 'Пароль' }).click();
@@ -14,10 +14,10 @@ import { test, expect } from '@playwright/test';
 
 test('test-show', async ({ page }) => {
   await test.step('Go to main page site', async () => {
-    await page.goto('https://wife-recipes.vercel.app');
+    await page.goto('/');
   });
   await test.step('Icon logo and button "Добавить рецепт" is visible', async () => {
     await expect(page.getByTestId('app-home')).toBeVisible();
-    await expect(page.getByLabel('Добавить рецепт')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Добавить рецепт' })).toBeVisible();
   });
 });
